84e8224bbe259dabe0c019c12821d91ddaf2979d
Paaugusto3 <paulinhoaugusto01@outlook.com>
2024-10-21 21:08:56 -0300

    Subir o site para o git

---
+++ index.js
@@ -0,0 +1,14 @@
+let botao = document.getElementById ("botao-pergunta")
+let menu = document.getElementById("pergunta")
+
+function abacate(){
+   if( menu.style.display == "none") {
+    menu.style.display = "block"
+    } else { 
+menu.style.display = "none"
+    }
+}
+
+botao.addEventListener("click", abacate)
+
+console.log("hello")
